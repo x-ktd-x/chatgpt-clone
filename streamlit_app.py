11c17138c2e90af3a222917f19fa06d25aa3c2a8
HiroakiKatada <hiroaki.katada@steamship.co.jp>
2024-02-19 12:26:54 +0900

    :wrench:[add] OAuth対応

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,41 @@
-# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
-
 import openai
 import streamlit as st
+from google_oauth2_required import google_oauth2_required
 
-with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('APIキーの取得に成功しました!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        st.warning('APIキーの取得に失敗しました', icon='⚠️')
-        # openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        # if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-        #     st.warning('Please enter your credentials!', icon='⚠️')
-        # else:
-        #     st.success('Proceed to entering your prompt message!', icon='👉')
+# main
+@google_oauth2_required
+def main():
+    with st.sidebar:
+        st.title('🤖💬 OpenAI Chatbot')
+        # 本番
+        if 'OPENAI_API_KEY' in st.secrets:
+            st.success('APIキーの取得に成功しました!', icon='✅')
+            openai.api_key = st.secrets['OPENAI_API_KEY']
+        else:
+            st.warning('APIキーの取得に失敗しました', icon='⚠️')
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model="gpt-4-1106-preview",
+                messages=[{"role": m["role"], "content": m["content"]}
+                          for m in st.session_state.messages], stream=True):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+# メイン関数の実行
+main()
